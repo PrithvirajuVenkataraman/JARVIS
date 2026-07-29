c4785a541e9f8f5e277460925844b77f4cba3ee6
PrithvirajuVenkataraman <147894867+PrithvirajuVenkataraman@users.noreply.github.com>
2026-07-29 07:28:34 +0530

    Delete api/_lib/types/latest-sources.ts

--- api/_lib/types/latest-sources.ts
+++
@@ -1,8 +0,0 @@
-export type LatestSource = {
-    id: string;
-    name: string;
-    url: string;
-    category: string;
-};
-
-export { LATEST_SOURCES, getLatestSources } from '../latest/latest-sources.js';
